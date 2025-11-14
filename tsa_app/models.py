fe5b59d09a32a4eafef76c08e7cb3665afee54d4
Alexandr Pavlovich <amator4x@gmail.com>
2025-11-14 20:37:24 +0300

    UPD Made the recordings of materials unique in the models

--- tsa_app/models.py
+++ tsa_app/models.py
@@ -66,10 +66,9 @@ class Worker(models.Model):
 
 
 class Material(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     price = models.FloatField(null=True, blank=True)
     build_obj = models.ManyToManyField('BuildObject', related_name='materials')
-    # price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
         return self.name
